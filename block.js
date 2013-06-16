5a8a5b5956e90ed9613c2cf2f497ebf3b023ee7f
Brendan McLoughlin <bmac325@gmail.com>
2013-06-16 02:33:59 -0400

    set block's image and add it to the scene

--- block.js
+++ block.js
@@ -6,6 +6,10 @@ require(['jam'], function(jam){
     var game = jam.Game(500, 300, document.body);
     var block = jam.Sprite(10, 100);
 
+    block.setImage('data:image/gif;base64,R0lGODlhAQABAPAAAP8AAP///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==', 100, 100);
+    game.root.scene.add(block);
+    game.run();
     window.game = game;
     window.block = block;
+
 });
--- jam/jam/game.js
+++ jam/jam/game.js
@@ -5,7 +5,6 @@ define(["vector", "sprite", "input", "util"], function(Vector, Sprite, Input, Ut
 		self._canvas = document.createElement("canvas");
 		// style attributes for doing "pixel-zoom" (nearest neighbor)
 		self._canvas.setAttribute("style","image-rendering: -moz-crisp-edges; image-rendering: -o-crisp-edges; image-rendering:-webkit-optimize-contrast; -ms-interpolation-mode:nearest-neighbor;");
-		self._canvas.style.position = "relative";
 		self._canvas.style.border = "1px solid black";
 		self._context = self._canvas.getContext("2d");
 		self.hasFocus = false;
@@ -32,8 +31,6 @@ define(["vector", "sprite", "input", "util"], function(Vector, Sprite, Input, Ut
 
 		// Always keep the canvas in the middle of the parent element
 		var onresize = function(){
-			self._canvas.style.left = (parentElement.clientWidth / 2 - (self.width * self.zoom) / 2) + "px";
-			self._canvas.style.top = (parentElement.clientHeight / 2 - (self.height * self.zoom) / 2) + "px";
 			self._canvas.style.width = (self.width * self.zoom) + "px";
 			self._canvas.style.height = (self.height * self.zoom) + "px";
 		};
